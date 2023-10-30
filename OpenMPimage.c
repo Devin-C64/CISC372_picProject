137d2d4fc13df3369059b0b1e9838514010d4f67
Devin <devinjc@udel.edu>
2023-10-30 18:29:01 -0400

    Attempted pragma fix

--- OpenMPimage.c
+++ OpenMPimage.c
@@ -60,14 +60,14 @@ uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm){
 //Returns: Nothing
 void convolute(Image* srcImage,Image* destImage,Matrix algorithm){
     int row,pix,bit;
-    # pragma omp for private(row,pix,bit) schedule(dynamic)
-        for (row=0;row<srcImage->height;row++){
-            for (pix=0;pix<srcImage->width;pix++){
-                for (bit=0;bit<srcImage->bpp;bit++){
-                    destImage->data[Index(pix,row,srcImage->width,bit,srcImage->bpp)]=getPixelValue(srcImage,pix,row,bit,algorithm);
-                }
+    for (row=0;row<srcImage->height;row++){
+        for (pix=0;pix<srcImage->width;pix++){
+            # pragma omp for private(bit)
+            for (bit=0;bit<srcImage->bpp;bit++){
+                destImage->data[Index(pix,row,srcImage->width,bit,srcImage->bpp)]=getPixelValue(srcImage,pix,row,bit,algorithm);
             }
         }
+    }
     
     
 }
