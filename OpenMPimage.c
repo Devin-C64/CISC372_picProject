82ac2517bff01eefb87da44e819180331af907ba
Devin <devinjc@udel.edu>
2023-10-30 18:35:40 -0400

    Revert

--- OpenMPimage.c
+++ OpenMPimage.c
@@ -60,9 +60,9 @@ uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm){
 //Returns: Nothing
 void convolute(Image* srcImage,Image* destImage,Matrix algorithm){
     int row,pix,bit;
+    # pragma omp for private(row,pix,bit)
     for (row=0;row<srcImage->height;row++){
         for (pix=0;pix<srcImage->width;pix++){
-            # pragma omp for private(bit)
             for (bit=0;bit<srcImage->bpp;bit++){
                 destImage->data[Index(pix,row,srcImage->width,bit,srcImage->bpp)]=getPixelValue(srcImage,pix,row,bit,algorithm);
             }
